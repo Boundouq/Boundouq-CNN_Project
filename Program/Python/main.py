31c1013f74147e652bc5940a3f56673b4ce41927
BOUNDOUQ <boundouq.ayoub.tsi@gamil.com>
2020-12-06 12:55:44 +0100

    C++_part

---
+++ Program/Python/main.py
@@ -0,0 +1,168 @@
+#####################################################################################
+###                                    CNN_Project                                ###
+#####################################################################################
+
+import normalization
+import convolution
+import maxpool
+
+import png
+from math import sqrt
+import numpy as np
+
+# The number of the picture that we will normalize
+IMAGE_NUM = 7
+
+# Convolution stage between 0 and 2
+STAGE = 5
+
+#The output image size
+X = [24, 12, 6]
+W = [3, 64, 32, 180]
+B = [64, 32, 20, 10]
+
+# Input files
+image_source = open("data/test_batch.bin", "rb")
+kernel = open('data/CNN_coeff_3x3_pro.txt', "r")
+
+# Output files
+image_name = 'Results/Image_' + str(IMAGE_NUM) + '.png'
+image_matrix = 'Results/Normalization_Image_' + str(IMAGE_NUM) + '.txt'
+image_convolution_64 = 'Results/Convolution_Image_'+ str(IMAGE_NUM) + '_' + str(X[0]) + '.txt'
+image_maxpool_64 = 'Results/Maxpool_Image_'+ str(IMAGE_NUM) + '_' + str(B[0]) + '.txt'
+image_convolution_32 = 'Results/Convolution_Image_' + str(IMAGE_NUM) + '_' + str(X[1]) + '.txt'
+image_maxpool_32 = 'Results/Maxpool_Image_' + str(IMAGE_NUM) + '_' + str(B[1]) + '.txt'
+image_convolution_20 = 'Results/Convolution_Image_'+ str(IMAGE_NUM) + '_' + str(X[2]) + '.txt'
+image_maxpool_20 = 'Results/Maxpool_Image_'+ str(IMAGE_NUM) + '_' + str(B[2]) + '.txt'
+
+
+image_source.read(IMAGE_NUM * 3073 )
+print ("classe " +str(int.from_bytes(image_source.read(1), byteorder='big')))
+#matrix = open(image_matrix, 'r')
+
+# Normalised and non-normalised RGB list
+# red = list()
+non_red = list()
+# green = list()
+non_green = list()
+# blue = list()
+non_blue = list()
+# rgb = list()
+# non_rgb = list()
+
+# Parameters
+# input_64 = list()
+# input_32 = list()
+# input_20 = list()
+# biases_64 = list()
+# weights_64 = list()
+# biases_32 = list()
+# weights_32 = list()
+# biases_20 = list()
+# weights_20 = list()
+# b = list()
+# w = list()
+
+# Output convolution matrix
+# out_convolution_64 = list()
+# out_convolution_32 = list()
+# out_convolution_20 = list()
+
+
+#####################################################################################
+# Normalize
+red, non_red, green, non_green, blue, non_blue  = normalization.normalize(image_source)
+# green, non_green = normalization.normalize(image_source)
+# blue, non_blue = normalization.normalize(image_source)
+
+# Create image
+rgb, non_rgb = normalization.RGB_image(red, non_red, green, non_green, blue, non_blue)
+
+# Create normalized image matrix
+normalization.matrix(image_matrix,rgb)
+
+# Create non-normalized image
+normalization.image(image_name,non_rgb)
+
+#####################################################################################
+
+# Get kernel matrix (biases and weights), for convolution nember "stage"
+biases_64, weights_64 = convolution.get_kernel_matrix(kernel,0,B , W)
+
+# Get image matrix
+# convolution.get_matrix(image_matrix, 0, X,input_64)
+
+# Calculate convolution
+out_convolution_64 = convolution.convolution(rgb ,0,weights_64,biases_64, B, X)
+
+# Create output convolution matrix file
+convolution.convo_matrix(image_convolution_64, out_convolution_64, B, X, 0)
+
+# Get convulution matrix
+# tab_64 = maxpool.convert2array(image_convolution_64,B[0])
+
+# Maxpool_64
+tab_out_64 = maxpool.maxpool(out_convolution_64,image_maxpool_64)
+
+#####################################################################################
+
+# Get kernel matrix (biases and weights), for convolution nember "stage"
+biases_32, weights_32 = convolution.get_kernel_matrix(kernel,1,B , W)
+
+# Get image matrix
+#convolution.get_matrix(image_maxpool_64, 1, X,input_32)
+
+# Calculate convolution
+out_convolution_32 = convolution.convolution(tab_out_64,1,weights_32,biases_32, B, X)
+
+# Create output convolution matrix file
+convolution.convo_matrix(image_convolution_32, out_convolution_32, B, X, 1)
+
+# Get convulution matrix
+# tab_32 = maxpool.convert2array(image_convolution_32,B[1])
+
+# Maxpool_32
+tab_out_32 = maxpool.maxpool(out_convolution_32,image_maxpool_32)
+
+#####################################################################################
+
+# Get kernel matrix (biases and weights), for convolution nember "stage"
+biases_20, weights_20 = convolution.get_kernel_matrix(kernel, 2,B , W)
+
+# Get image matrix
+# convolution.get_matrix(image_maxpool_32, 2, X,input_20)
+
+# Calculate convolution
+out_convolution_20 = convolution.convolution(tab_out_32,2,weights_20,biases_20, B, X)
+
+# Create output convolution matrix file
+convolution.convo_matrix(image_convolution_20, out_convolution_20, B, X, 2)
+
+# Get convulution matrix
+# tab_20 = maxpool.convert2array(image_convolution_20,B[2])
+
+# Maxpool_20
+tab_out_20 = maxpool.maxpool(out_convolution_20,image_maxpool_20)
+
+#####################################################################################
+
+# Reshape
+vecteur = maxpool.reshapee(tab_out_20)
+
+# Get kernel matrix (biases and weights), for convolution nember "stage"
+biases_f, weights_f = maxpool.get_coeff(kernel, 3, B, W)
+
+# Fully connected
+result = maxpool.fully_conn(weights_f, vecteur,biases_f)
+
+print(result)
+
+tab = []
+for elm in result:
+    tab.append(elm)
+t = []
+for i in range (9):
+    a = float(max(tab))
+    t.append(tab.index(a))
+    tab[tab.index(a)] = -100
+print (t)
---
+++ Program/Python/normalization.py
@@ -0,0 +1,157 @@
+#####################################################################################
+###                                     NORMALISE                                 ###
+#####################################################################################
+
+import png
+from math import sqrt
+#import numpy as np
+
+# The number of the picture that we will normalize
+#IMAGE_NUM = 5
+
+# Output files
+#image_name = 'Ima_' + str(IMAGE_NUM) + '.png'
+#image_matrix = 'Normalization_Image_' + str(IMAGE_NUM) + '.txt'
+
+#image_source = open("image.bin", "rb")
+
+# Read the picture number IMAGE_NUM
+#image_source.read(IMAGE_NUM * 3073 + 1)
+
+# Normalised and non-normalised RGB list
+#red = list()
+#non_red = list()
+#green = list()
+#non_green = list()
+#blue = list()
+#non_blue = list()
+#rgb = list()
+#non_rgb = list()
+
+#####################################################################################
+
+# Normalization
+def normalize(image):
+    sum = 0
+    average = 0
+    deviation = 0
+    size = 24 * 24 * 3
+    nor_list = []
+    non_nor_list = []
+    red = list()
+    non_red = list()
+    green = list()
+    non_green = list()
+    blue = list()
+    non_blue = list()
+    # Cut out image
+    zeros = [0 for i in range(26)]
+    for l in range (3):
+        c = []
+        n_c = []
+        c.append(zeros)
+        for i in range(0, 32):
+            if i > 3 and i < 28:
+                l = []
+                n_l = []
+                l.append(0)
+                for j in range(0, 32):
+                    if j > 3 and j < 28:
+                        a = int.from_bytes(image.read(1), byteorder='big')
+                        assert a <= 255
+                        l.append(a)
+                        n_l.append(a)
+                    else:
+                        image.read(1)
+                l.append(0)
+
+                c.append(l)
+                n_c.append(n_l)
+            else:
+                image.read(32)
+        c.append(zeros)
+        nor_list.append(c)
+        non_nor_list.append(n_c)
+    # Calculate average value
+    for l in range (3):
+        for i in range(1, 25):
+            for j in range(1, 25):
+                sum += nor_list[l][i][j]
+    average = sum / size
+
+    # Calculate standard
+    for l in range(3):
+        for i in range(1, 25):
+            for j in range(1, 25):
+                deviation += (nor_list[l][i][j] - average) ** 2
+    deviation = sqrt(deviation / size)
+
+    # Normalization
+    for l in range(3):
+        for i in range(1, 25):
+            for j in range(1, 25):
+                nor_list[l][i][j] = (nor_list[l][i][j] - average) / \
+                    max(deviation, 1/sqrt(size))
+    red = nor_list[0]
+    non_red = non_nor_list[0]
+    green = nor_list[1]
+    non_green = non_nor_list[1]
+    blue = nor_list[2]
+    non_blue = non_nor_list[2]
+    return (red, non_red, green, non_green, blue, non_blue)
+
+####################################################################################
+
+# Create image
+def RGB_image(lred, lnon_red, lgreen, lnon_green, lblue, lnon_blue):
+    lnon_rgb = []
+    lrgb = []
+    for i in range(0, 24):
+        ln = []
+        for j in range(0, 24):
+            # Non-normalized image
+            ln.append(lnon_red[i][j])
+            ln.append(lnon_green[i][j])
+            ln.append(lnon_blue[i][j])
+        lnon_rgb.append(ln)
+
+    # Normalized image
+    lrgb.append(lred)
+    lrgb.append(lgreen)
+    lrgb.append(lblue)
+    return (lrgb, lnon_rgb)
+
+####################################################################################
+
+# Create normalized image matrix
+def matrix(image_matrix, rgb_list):
+    m = open(image_matrix, "w")
+    for y in range(0, 3):
+        for i in range(0, 26):
+            for j in range(0, 26):
+                m.write(str(rgb_list[y][i][j]) + ' ')
+            m.write('\n')
+        m.write('\n')
+    m.close()
+
+####################################################################################
+
+# Create non-normalized image
+def image(image_name, n_rgb):
+    im = open(image_name, 'wb')
+    w = png.Writer(24, 24, greyscale=False)
+    w.write(im, n_rgb)
+    im.close()
+
+####################################################################################
+
+
+#normalize(image_source, red, non_red)
+#normalize(image_source, green, non_green)
+#normalize(image_source, blue, non_blue)
+
+#RGB_image(red, non_red, green, non_green, blue, non_blue, rgb, non_rgb)
+
+#matrix(image_matrix, rgb)
+
+#image(image_name, non_rgb)
